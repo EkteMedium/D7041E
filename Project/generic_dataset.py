194941c6b9e647b2fb11aad7c7e879e42737bcb4
EkteMedium <sanano-1@student.ltu.se>
2024-11-20 15:34:58 +0100

    Added preprocessing function

--- Project/generic_dataset.py
+++ Project/generic_dataset.py
@@ -4,6 +4,7 @@ Contaions functionallity for handeling datasets.
 import random
 from ucimlrepo import fetch_ucirepo
 import numpy as np
+import pandas as pd
 
 class GenericDataset():
     '''
@@ -17,54 +18,57 @@ class GenericDataset():
       ValueError: If the dataset is not meant for classification.
 
     '''
-    def __init__(self, uci_id, train_split=1, val_split=0, seed = None, show_info = None):
+    def __init__(self, uci_id, splits = (1,0,0), seed = None, show_info = False):
 
         # Sets seed
         random.seed(seed)
 
-        # Calculate test_split
-        test_split = 1-train_split-val_split
-
         # Raise error if splits wrong
-        if test_split < 0 or test_split > 1:
-            raise ValueError(f"Test split should be between 0 and 1, currently {test_split}.")
+        if splits[0] < 0 or splits[0] > 1:
+            raise ValueError(f"Train split should be between 0 and 1, currently {splits[0]}.")
 
-        if train_split < 0 or train_split > 1:
-            raise ValueError(f"Train split should be between 0 and 1, currently {train_split}.")
+        if splits[1] < 0 or splits[1] > 1:
+            raise ValueError(f"Validation split should be between 0 and 1, currently {splits[1]}.")
 
-        if val_split < 0 or val_split > 1:
-            raise ValueError(f"Validation split should be between 0 and 1, currently {val_split}.")
+        if splits[2] < 0 or splits[2] > 1:
+            raise ValueError(f"Test split should be between 0 and 1, currently {splits[2]}.")
 
-        # save splits.
-        self.train_split = train_split
-        self.val_split = val_split
-        self.test_split = test_split
+        if sum(splits)!=1:
+            raise ValueError("Splits should add to 1.")
 
-        # fetch dataset
+        # Save splits.
+        self.train_split = splits[0]
+        self.val_split = splits[1]
+        self.test_split = splits[2]
+
+        # Fetch dataset
         self.dataset = fetch_ucirepo(id=uci_id)
 
-        print(type(self.dataset.data.features))
-        print(type(self.dataset.data.targets))
+        # Sets flag indicating if the database has targets.
+        self.has_targets = self.dataset.data.targets is not None
 
-        tup = self._split_pandas(self.dataset.data.features,train_split,val_split,test_split)
-        print("\n"*5)
-        print(tup[0])
-        print("\n"*5)
-        print(tup[1])
-        print("\n"*5)
-        print(tup[2])
-        print("\n"*5)
+        df = self.dataset.data.features
+        print(df)
+        print(df.dtypes)
+        df = self._pre_process(self.dataset.data.features)
+        print(df)
+        print(df.dtypes)
 
-        # check if Classification is one of the tasks.
+        # Split into train/val/test splits.
+        self.features_tuple, self.targets_tuple = self._split_pandas(self.dataset.data.features,
+                                                                     self.dataset.data.targets,
+                                                                     self.train_split,
+                                                                     self.val_split,
+                                                                     self.test_split)
+
+        # Check if Classification is one of the tasks.
         if 'Classification' not in self.dataset.metadata['tasks']:
             raise ValueError((f'This dataset is not suitable for Classification. '
                               f'Only suitable for {str(self.dataset.metadata['tasks'])}.'))
+        
 
-        # sets flag indicating if the database has targets.
-        self.has_targets = self.dataset.data.targets is not None
-
-        # Print info
-        if show_info is True:
+        # Print info.
+        if show_info:
             print(self.__str__())
 
 
@@ -94,14 +98,29 @@ class GenericDataset():
         """
         return self.dataset.metadata
 
-    def get_features(self):
+    def get_train(self):
         """
-        Returns a numpy of the dataset features
+        Returns a numpy of the train features
         """
-        feat = self.dataset.data.features
-        print(feat)
-        print(feat.dtypes)
-        return np.array(feat)
+        features = self.features_tuple[0]
+        targets = self.targets_tuple[0]
+        return np.array(features), np.array(targets)
+
+    def get_validation(self):
+        """
+        Returns a numpy of the train features
+        """
+        features = self.features_tuple[1]
+        targets = self.targets_tuple[1]
+        return np.array(features), np.array(targets)
+
+    def get_test(self):
+        """
+        Returns a numpy of the train features
+        """
+        features = self.features_tuple[2]
+        targets = self.targets_tuple[2]
+        return np.array(features), np.array(targets)
 
     def get_labels(self):
         """
@@ -109,11 +128,11 @@ class GenericDataset():
         """
         return self.dataset.data.targets
 
-    def _split_pandas(self, data, *splits):
+    def _split_pandas(self, features, targets, *splits):
         if sum(splits)!=1:
             raise ValueError("Splits should add to 1.")
 
-        num_rows = len(data.index)
+        num_rows = len(features.index)
 
         indexes = list(range(num_rows))
         split_indexes = []
@@ -125,11 +144,35 @@ class GenericDataset():
             split_indexes.append(inxs)
         split_indexes.append(indexes)
 
-        data_splits = []
+        feature_splits = []
+        target_splits = []
         for indexes in split_indexes:
-            data_splits.append(data.iloc[indexes])
+            feature_splits.append(features.iloc[indexes])
+            target_splits.append(targets.iloc[indexes])
 
-        return tuple(data_splits)
+        return (tuple(feature_splits), tuple(target_splits))
+
+    def _pre_process(self,df):
+        df = self._normalize(df)
+        df = self._one_hot_encode(df)
+        return df
+
+    def _one_hot_encode(self,df:pd.DataFrame):
+        object_columns = [col for col in df if df[col].dtype=='O']
+        df2 = pd.get_dummies(df, columns=object_columns, dtype=float).astype(float)
+        return df2
+
+    def _normalize(self, df, means=None, stds=None):
+        non_O_columns = [col for col in df if df[col].dtype!='O']
+
+        #if non_O_columns != len(means) or non_O_columns != len(stds):
+        #    raise ValueError("Length of means and stds need to be the same as number of non object columns.")
+
+        # Whitening
+        for column in non_O_columns:
+            df[column] = (df[column]-df[column].mean())/df[column].std()
+        
+        return df
 
 if __name__ == "__main__":
-    ds = GenericDataset(53, train_split=0.5, val_split=0.3, show_info=True, seed=1234)
+    ds = GenericDataset(53, splits=(0.5, 0.3, 0.2), show_info=True, seed=1234)
